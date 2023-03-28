02e12e62692a3383306601b2e2be5b6af6239bcf
Luc Lefebure <126509340+L-L-42@users.noreply.github.com>
2023-03-28 10:22:16 +0200

    correction app.jsx and add import App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import PokemonCard from './components/Pokemoncard.jsx';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 const App = () => {
   return (
