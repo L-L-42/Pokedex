43ecc3963baf2bc830c7b2292bf6b937c4ea757d
Luc Lefebure <126509340+L-L-42@users.noreply.github.com>
2023-03-29 12:29:26 +0200

    create branch prop.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PokemonCard from "./components/Pokemoncard.jsx";
 import { useState } from "react";
-
+import PropTypes from "prop-types";
 import "./App.css";
 
 const App = () => {
@@ -30,4 +30,5 @@ const pokemonList = [
     name: "mew",
   },
 ];
+
 export default App;
