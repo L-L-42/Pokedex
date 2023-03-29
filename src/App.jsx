032d788b39d0536b135f2d27b9fc179f0b074a6b
Luc Lefebure <126509340+L-L-42@users.noreply.github.com>
2023-03-29 14:04:43 +0200

    create propTypes.shape for PokemonCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import PokemonCard from "./components/Pokemoncard.jsx";
 import { useState } from "react";
-import PropTypes from "prop-types";
 import "./App.css";
 
 const App = () => {
--- src/components/Pokemoncard.jsx
+++ src/components/Pokemoncard.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import PropTypes from "prop-types";
 
 const PokemonCard = ({ pokemon }) => {
   return (
     <figure>
       {pokemon.imgSrc ? (
-        <img src={pokemon.imgSrc} alt="pokemon.name" />
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
       ) : (
         <p>???</p>
       )}
       <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
+};
+
+PokemonCard.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imgSrc: PropTypes.string,
+  }).isRequired,
 };
 
 //function PokemonCard (props){
